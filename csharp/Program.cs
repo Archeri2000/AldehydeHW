281b3513af458fd2ff195deba5f996b784f5ff2d
archery2000 <archery2000@users.noreply.github.com>
2019-07-13 01:09:52 +0800

    Completed C#

---
+++ csharp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader input = new StreamReader(args[0]);
+            StreamWriter output = new StreamWriter(args[1]);
+            string line;
+            while((line = input.ReadLine()) != null)
+            {
+                Console.WriteLine(CapsFirst(line));
+                output.WriteLine(CapsFirst(line));
+            }
+            input.Close();
+            output.Close();
+        }
+
+        static string CapsFirst(string input)
+        {
+            StringBuilder builder = new StringBuilder(input);
+            bool CapsNext = true;
+            for (int i = 0; i < builder.Length; i++)
+            {
+                if (CapsNext)
+                {
+                    CapsNext = false;
+                    if (builder[i] >= 97 && builder[i] <= 122)
+                    {
+                        builder[i] = (char)(builder[i] - 32);
+                    }
+                }
+
+                if (builder[i] == ' ' || builder[i] == '\n')
+                {
+                    CapsNext = true;
+                }
+            }
+
+            return builder.ToString();
+        }
+    }
+}
