3bcd41d8ed09940852fafa9a75b73e37fb4d2e34
archery2000 <archery2000@users.noreply.github.com>
2019-07-30 21:52:43 +0800

    Compelted Typescript HW2

--- typescript_server/server.js
+++ typescript_server/server.js
@@ -1,19 +1,37 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 // NPM has been set up to import capitalise as a module from the first assignment
-var caps = require("capitalise_js");
-var Http = module;
-('http');
-var CapsServer = /** @class */ (function () {
-    function CapsServer() {
-        this.header = { 'Content-Type': 'text/plain' };
-        var server = Http.createServer(this.onRequest);
+var caps = require("capitalise_js/capitalise.js");
+var express_1 = __importDefault(require("express")); /*
+class CapsServer {
+    private header:any = {'Content-Type': 'text/plain'};
+
+    constructor() {
+        var server:http.Server = http.createServer(this.onRequest);
         server.listen(3000);
         console.log("Server starting..");
     }
-    CapsServer.prototype.onRequest = function (request, response) {
+
+    private onRequest(request:http.IncomingMessage, response:http.ServerResponse):void {
         response.writeHead(200, this.header);
+        request.on('data',(message)=>{response.write()})
         response.end("Hello TypeScript & node.js");
-    };
-    return CapsServer;
-}());
-var server = new CapsServer();
+    }
+}
+
+var server = new CapsServer();*/
+var server = express_1.default();
+var port = 3000;
+// define a route handler for the default home page
+server.post("/", function (req, res) {
+    var body = "";
+    req.on("data", function (dat) { body += dat; });
+    req.on("end", function () { res.send(caps.CapsFirst(body)); });
+});
+// start the Express server
+server.listen(port, function () {
+    console.log("server started at http://localhost:" + port);
+});
--- typescript_server/server.ts
+++ typescript_server/server.ts
@@ -1,24 +1,38 @@
 
 
 // NPM has been set up to import capitalise as a module from the first assignment
-const caps = require("capitalise_js");
-
-import Http = module('http');
-
-
+const caps = require("capitalise_js/capitalise.js");
+import * as http from "http";
+import express from "express";/*
 class CapsServer {
-    private header:Object = {'Content-Type': 'text/plain'};
+    private header:any = {'Content-Type': 'text/plain'};
 
     constructor() {
-        var server:Http.Server = Http.createServer(this.onRequest);
+        var server:http.Server = http.createServer(this.onRequest);
         server.listen(3000);
         console.log("Server starting..");
     }
 
-    private onRequest(request:Http.ServerRequest, response:Http.ServerResponse):void {
+    private onRequest(request:http.IncomingMessage, response:http.ServerResponse):void {
         response.writeHead(200, this.header);
+        request.on('data',(message)=>{response.write()})
         response.end("Hello TypeScript & node.js");
     }
 }
 
-var server = new CapsServer();
+var server = new CapsServer();*/
+
+var server = express();
+const port = 3000;
+
+// define a route handler for the default home page
+server.post( "/", ( req: express.Request, res: express.Response) => {
+    let body = "";
+    req.on("data", dat =>{body += dat});
+    req.on("end", ()=>{res.send(caps.CapsFirst(body))});
+} );
+
+// start the Express server
+server.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
